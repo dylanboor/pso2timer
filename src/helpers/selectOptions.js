d5135491a4ac25f0fff3fe2572d08eeb836039ed
dylanboor <dylan.j.boor@gmail.com>
2022-01-17 23:05:13 -0600

    fix: Svia->Svac

--- src/helpers/selectOptions.js
+++ src/helpers/selectOptions.js
@@ -9,6 +9,6 @@ export const selectOptions = [
   { name: "W: Kelkundo", value: "West_Kelkundo" }, 
   { name: "C: Nogleth", value: "Central_Nogleth" }, 
   { name: "C: Fortos", value: "Central_Fortos" }, 
-  { name: "C: Svia", value: "Central_Svia" }, 
+  { name: "C: Svac", value: "Central_Svac" }, 
   { name: "C: Renus", value: "Central_Renus" }, 
 ];
