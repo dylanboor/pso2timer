107a940c7d81b6c8b72e04ed3b1d68494385a9c2
Dylan Boor <Dylan.j.boor@gmail.com>
2022-02-05 18:15:46 -0500

    test: initial test cases. wip for field input

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,37 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import constants from './constants.json';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App unit tests', () => {
+  beforeEach(() => {
+    render(<App />);
+  });
+
+  test('renders title of the app', () => {
+    const e = screen.getByText(constants.TITLE_TEXT);
+    expect(e).toBeInTheDocument();
+  });
+
+  test('renders all input elements', () => {
+    let blockTextInput = screen.getByLabelText(constants.BLOCK_TEXT_INPUT_LABEL);
+    let timerSelectInput = screen.getByLabelText(constants.TIMER_SELECT_INPUT_LABEL);
+    let startButton = screen.getByText(constants.START_BUTTON_TEXT);
+    let deadButton = screen.getByText(constants.DEAD_BUTTON_TEXT);
+
+    expect(blockTextInput).toBeInTheDocument();
+    expect(timerSelectInput).toBeInTheDocument();
+    expect(startButton).toBeInTheDocument();
+    expect(deadButton).toBeInTheDocument();
+  })
+
+  test('creates a timer when Start is clicked', () => {
+    let blockTextInput = screen.getByLabelText(constants.BLOCK_TEXT_INPUT_LABEL);
+    let timerSelectInput = screen.getByLabelText(constants.TIMER_SELECT_INPUT_LABEL);
+    let startButton = screen.getByText(constants.START_BUTTON_TEXT);
+    let deadButton = screen.getByText(constants.DEAD_BUTTON_TEXT);
+
+    //blockTextInput do stuff
+    deadButton.click();
+  });
+
+})
