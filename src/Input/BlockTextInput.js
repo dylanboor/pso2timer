31f5b44fd5e4de96258cf8cf81f228fb13702237
dylanboor <dylan.j.boor@gmail.com>
2022-10-10 21:44:22 -0500

    feat: change block to number input

--- src/Input/BlockTextInput.js
+++ src/Input/BlockTextInput.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import constants from '../constants.json';
+import React from "react";
+import constants from "../constants.json";
 
 export default function BlockTextInput({ handleChange, ...props }) {
   return (
     <div>
       <label>
         {constants.BLOCK_TEXT_INPUT_LABEL}&nbsp;
-        <input type="text" onChange={(e) => handleChange('block', e.target.value)} {...props} />
+        <input
+          type="number"
+          onChange={(e) => handleChange("block", e.target.value)}
+          {...props}
+        />
       </label>
       <br />
     </div>
